0ebf4d8f6fce3bf0f335f99116b47e14999ddb56
Binner <lucian.binner@hotmail.com>
2021-02-27 13:56:05 -0300

    typeorm implemented

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,38 @@
 import express from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from './models/Orphanage';
+
+import './database/connection'
 
 const app = express();
+app.use(express.json());
 
-app.get('/users', (resquest, response) => {
-    return response.send('Hello World')
+app.post('/orphanages', async (req, res) => {
+    const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    } = req.body;
+
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends
+    });
+
+    await orphanagesRepository.save(orphanage);
+
+    return res.json({ message: 'ok' })
 })
 
 app.listen(3333);
