b20368ceb612f4dbc2ff3ac5ddc70d919d3a97bc
Binner <lucian.binner@hotmail.com>
2021-02-27 18:21:52 -0300

    backend finish

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import path from 'path';
 import 'express-async-errors';
 
@@ -10,6 +11,7 @@ import './database/connection'
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
