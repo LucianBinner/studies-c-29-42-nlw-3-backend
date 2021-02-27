abad5107a01f98215daf2927250204526c1ee957
Binner <lucian.binner@hotmail.com>
2021-02-27 17:44:57 -0300

    exceptions

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import 'express-async-errors';
 
 import routes from './routes';
 
+import errorHandler from './errors/handler';
+
 import './database/connection'
 
 const app = express();
@@ -11,6 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler);
 
 
 app.listen(3333);
